0002868113ef2bac3130d01bfaaf3f01fff39e24
Marcus Melgaço <marcus.melgaco@infosistemas.com.br>
2021-05-16 16:14:33 -0300

    Alterações

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const database = require('./database');
 const app = express()
 const PortalTransparenciaController = require('./src/controllers/portalTransparenciaController')
 
-app.post('/', async (req, res) =>  {
+app.post('/postDadosPortalTransparencia', async (req, res) =>  {
   try{
     let portalTransparenciaController =  new PortalTransparenciaController();
-    portalTransparenciaController.postPortalTransparencia();
+    await portalTransparenciaController.postPortalTransparencia();
+    res.status(200).send('Dados Adicionados Com Sucesso!')
   }catch(err){
     res.status(500).send('Falha ao Recuperar e Salvar Informações do Portal da Transparencia || Erro :' + err)
   }
@@ -18,7 +19,6 @@ async function start(){
     const port = config.get('SERVER').PORT;
     const db = config.get('CONNECTIONS').MONGO.DATABASE;
     const enviroment = config.get('ENV');
-
     app.listen(port ,() =>{
         console.log(`Servidor Rodando na porta ${port} || Env : ${enviroment} || Database : ${db}`)
 })
